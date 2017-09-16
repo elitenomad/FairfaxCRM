0e60adf41284ebfa409e75b56332895a1c0f6c59
Pranava Swaroop <pranava.swaroop@fairfaxmedia.com.au>
2017-09-16 12:04:53 +1000

    Add The version changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170915031437) do
+ActiveRecord::Schema.define(version: 20170915223652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 20170915031437) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["samurai_user_id"], name: "index_samurai_contacts_contacts_on_samurai_user_id"
+  end
+
+  create_table "samurai_tasks_tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "samurai_user_id"
+    t.bigint "samurai_contacts_contacts_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["samurai_contacts_contacts_id"], name: "index_samurai_tasks_tasks_on_samurai_contacts_contacts_id"
+    t.index ["samurai_user_id"], name: "index_samurai_tasks_tasks_on_samurai_user_id"
   end
 
   create_table "samurai_users", force: :cascade do |t|
@@ -46,4 +57,6 @@ ActiveRecord::Schema.define(version: 20170915031437) do
   end
 
   add_foreign_key "samurai_contacts_contacts", "samurai_users"
+  add_foreign_key "samurai_tasks_tasks", "samurai_contacts_contacts", column: "samurai_contacts_contacts_id"
+  add_foreign_key "samurai_tasks_tasks", "samurai_users"
 end
